978d7690f79dc2188858cab2e9f76cea78a9d264
Pravalika <Pravalika@DESKTOP-CJIU4FR>
2022-02-04 10:42:57 +0530

    Java programs print customerinformation(id,name,address) on 04-02-2022

---
+++ Javadatatypes/src/com/javadatatypes/Example.java
@@ -0,0 +1,20 @@
+package com.javadatatypes;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		
+		//** Write java program print customer information(id,name,address)
+		
+		int customerId = 7886;
+		String customerName = "Pravalika";
+		String customerAddress="Husnabad";
+		
+		System.out.println(customerId);
+		System.out.println(customerName);
+		System.out.println(customerAddress);
+
+	}
+
+}
